6cc5c7e225f3b57e378a0bfc83374e81c6d1d25b
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-04 13:44:15 +0530

    number of good pairs in an array

---
+++ number_of_good_pairs.cpp
@@ -0,0 +1,22 @@
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count =0;
+        for(int i =0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    count +=1;
+                }
+            }
+        }
+        return count;
+    }
+};
