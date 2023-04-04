e226714a60f1baf142dd2a79c27162f89ddcb640
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-04 13:58:10 +0530

    number of good pair in a ngiven array

--- number_of_good_pairs.cpp
+++ number_of_good_pairs.cpp
@@ -2,18 +2,22 @@
 
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 
-
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int numIdenticalPairs(vector<int>& nums) {
-        int count =0;
-        for(int i =0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    count +=1;
+    int numIdenticalPairs(vector<int> &nums)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count += 1;
                 }
             }
         }
---
+++ richest_customer_wealth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        vector<int> wealth_count(accounts.size());
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
