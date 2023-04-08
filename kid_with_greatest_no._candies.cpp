8d650f13c74395ca5ff2f547dc46239e29bdc947
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-08 17:12:58 +0530

    kid with greatest number of candies

---
+++ kid_with_greatest_no._candies.cpp
@@ -0,0 +1,37 @@
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has,
+//  and an integer extraCandies, denoting the number of extra candies that you have.
+
+// Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, 
+// they will have the greatest number of candies among all the kids, or false otherwise.
+
+// Note that multiple kids can have the greatest number of candies.
+
+
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+    {
+        int maximum = 0;
+        vector<bool> result;
+        for (int i = 0; i < candies.size(); i++)
+            maximum = max(candies[i], maximum);
+        for (int i = 0; i < candies.size(); i++)
+            candies[i] += extraCandies;
+
+        for (int i = 0; i < candies.size(); i++)
+        {
+            if (candies[i] >= maximum)
+                result.push_back(1);
+            else
+                result.push_back(0);
+        }
+        return result;
+    }
+};
