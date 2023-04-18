2d8b965efa3fabd670480ce629a8b69aa945f489
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-04-18 21:01:20 +0530

    array_easy

---
+++ height_checker.cpp
@@ -0,0 +1,33 @@
+// A school is trying to take an annual photo of all the students.
+// The students are asked to stand in a single file line in non-decreasing order by height.
+//  Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+// You are given an integer array heights representing the current order that the students are standing in.
+//  Each heights[i] is the height of the ith student in line (0-indexed).
+
+// Return the number of indices where heights[i] != expected[i].
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+expected = sort(heights.begin(), heights.end());
+int count = 0;
+for (int i = 0; i < heights.size(); i++)
+{
+    if(heights[i]==expected[i])
+    {
+        count+=1;
+    }
+    return count;
+}
+
+
+
+int main()
+{
+
+    return 0;
+}
---
+++ sort_array_by_parity.cpp
@@ -0,0 +1,25 @@
+// Given an integer array nums,
+// move all the even integers at the beginning of the array followed by all the odd integers.
+
+// Return any array that satisfies this condition
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortArrayByParity(vector<int> &nums)
+    {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] % 2 == 0)
+                result.insert(result.begin(), nums[i]);
+            else
+                result.push_back(nums[i]);
+        }
+        return result;
+    }
+};
