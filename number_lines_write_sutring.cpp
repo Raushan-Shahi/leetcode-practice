d2f6f3720d6e77d8896c3df170f696f450f9d3d7
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-07-02 21:59:36 +0530

    array easy

---
+++ number_lines_write_sutring.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> numberOfLines(vector<int>& widths, string s) {
+        for (int i = 0; i < s.size(); i++)
+        {
+            while ()
+            {
+                /* code */
+            }
+            
+        }
+        
+    }
+};
